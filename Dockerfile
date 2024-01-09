33fe37aa5e1f8201fc2ddcf58e7e87bf1bdc5782
Arjun Singh Kanwal <kanwal.arjun8900@gmail.com>
2024-01-09 20:53:47 +0530

    update

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17-alpine
+WORKDIR /opt
+#EVN PORT 8090
+EXPOSE 8090
+COPY target/*.jar /opt/app.jar
+#ENTRYPOINT exec java $JAVA_OPTS -jar app.jar
+ENTRYPOINT exec java $JAVA_OPTS  -cp app.jar org.example.Main
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true) {
+            System.out.println("Hello world! from arjun");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
